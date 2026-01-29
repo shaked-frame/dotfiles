ca1d6039b9a95e2d86cd471aa21c6d4af4049bf0
Shaked Hagag <shaked@frame.security>
2026-01-29 15:41:58 +0200

    equalize, maximize splits keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -17,6 +17,28 @@ keymap.set("n", "te", ":tabedit")
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
+-- Equalize splits
+keymap.set("n", "<leader>=", "<C-w>=", { desc = "Equalize splits" })
+
+-- Maximize buffer (toggle)
+local function toggle_maximize()
+  local current_win = vim.api.nvim_get_current_win()
+  local is_maximized = vim.w[current_win].maximized
+
+  if is_maximized then
+    -- Restore previous layout
+    vim.cmd("wincmd =")
+    vim.w[current_win].maximized = false
+  else
+    -- Maximize current window
+    vim.cmd("wincmd |")
+    vim.cmd("wincmd _")
+    vim.w[current_win].maximized = true
+  end
+end
+
+keymap.set("n", "<leader>m", toggle_maximize, { desc = "Maximize buffer (toggle)" })
+
 vim.keymap.set("n", "<C-S-M-A-\\>", function()
   vim.diagnostic.jump({ count = 1 })
 end, { desc = "Go to next diagnostic" })
